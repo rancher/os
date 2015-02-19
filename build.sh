6e87ea3981f01a1a252e3f03f022e72fd2a200d1
Darren Shepherd <darren@rancher.com>
2015-02-19 11:26:59 -0700

    Build script changes

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)
 
 export DOCKER_IMAGE=rancher-os-build
 
-./scripts/ci
+./scripts/ci "$@"
 mkdir -p dist
 docker run -it -e CHOWN_ID=$(id -u) -v $(pwd)/dist:/source/target $DOCKER_IMAGE
