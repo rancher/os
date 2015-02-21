3a574c32541b2cc9eaa2d605a3be686eb8eb2063
Darren Shepherd <darren@rancher.com>
2015-02-21 00:33:47 -0700

    Delete build install container

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ export DOCKER_IMAGE=rancher-os-build
 
 ./scripts/ci "$@"
 mkdir -p dist
-docker run -it -e CHOWN_ID=$(id -u) -v $(pwd)/dist:/source/target $DOCKER_IMAGE
+docker run --rm -it -e CHOWN_ID=$(id -u) -v $(pwd)/dist:/source/target $DOCKER_IMAGE
