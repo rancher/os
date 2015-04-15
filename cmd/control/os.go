b1c519ebe0269b6cfc9c7e681b3d3d364f75a29d
Darren Shepherd <darren@rancher.com>
2015-04-15 00:43:38 -0700

    Force os-installer container to use json-file logger

--- cmd/control/os.go
+++ cmd/control/os.go
@@ -162,6 +162,7 @@ func startUpgradeContainer(image string, stage, force bool) {
 
 	container := docker.NewContainer(config.DOCKER_SYSTEM_HOST, &config.ContainerConfig{
 		Cmd: "--name=os-upgrade " +
+			"--log-driver=json-file" +
 			"--rm " +
 			"--privileged " +
 			"--net=host " +
