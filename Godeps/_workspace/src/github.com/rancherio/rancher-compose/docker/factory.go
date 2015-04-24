5b54f458eac55c61056fc88c1a3a6c1183278119
deniseschannon <denise@rancher.com>
2015-04-23 22:31:11 -0700

    Added tty, stdinopen, workingdir for system-services

--- Godeps/_workspace/src/github.com/rancherio/rancher-compose/docker/factory.go
+++ Godeps/_workspace/src/github.com/rancherio/rancher-compose/docker/factory.go
@@ -36,6 +36,9 @@ func Convert(c *project.ServiceConfig) (*runconfig.Config, *runconfig.HostConfig
 		Image:        c.Image,
 		Labels:       kvListToMap(c.Labels),
 		ExposedPorts: ports,
+                Tty:          c.Tty,
+                OpenStdin:    c.StdinOpen,
+                WorkingDir:   c.WorkingDir,
 	}
 	host_config := &runconfig.HostConfig{
 		VolumesFrom: c.VolumesFrom,
