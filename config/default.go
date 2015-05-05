2bae134b3533971b5ae7284ede5c6ab638e80d3f
Darren Shepherd <darren@rancher.com>
2015-05-05 13:37:51 -0700

    Only bind mount /var/lib/rancher/conf

--- config/default.go
+++ config/default.go
@@ -179,7 +179,7 @@ func NewConfig() *Config {
 					SCOPE:       SYSTEM,
 				}),
 				Volumes: []string{
-					"/var/lib/rancher:/var/lib/rancher",
+					"/var/lib/rancher/conf:/var/lib/rancher/conf",
 					"/var/lib/docker:/var/lib/docker",
 					"/var/lib/system-docker:/var/lib/system-docker",
 				},
