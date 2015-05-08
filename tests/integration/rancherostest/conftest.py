f2d797d6cca0f27fde09112bb986d18d1acc0e5a
Ivan Mikushin <i.mikushin@gmail.com>
2015-05-08 10:42:18 +0500

    migrate to python-3.4

--- tests/integration/rancherostest/conftest.py
+++ tests/integration/rancherostest/conftest.py
@@ -3,6 +3,6 @@ import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
-def my_own_session_run_at_beginning():
+def chdir_to_project_root():
     os.chdir('../..')
     print('\nChdir to project root dir')
--- tests/integration/rancherostest/test_system.py
+++ tests/integration/rancherostest/test_system.py
@@ -1,12 +1,11 @@
 import pytest
-import string
 import subprocess
 import time
 
 
 @pytest.fixture(scope="module")
 def qemu(request):
-    p = subprocess.Popen('./scripts/run', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen('./scripts/run', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
     def fin():
         print('\nTerminating QEMU')
@@ -20,8 +19,8 @@ def qemu(request):
 @pytest.mark.timeout(20)
 def test_system_boot(qemu):
     for ln in iter(qemu.stdout.readline, ''):
-        ros_booted_substr = string.find(ln, 'RancherOS v0.3.1-rc2 started')
-        print(string.strip(ln))
+        ros_booted_substr = str.find(ln, 'RancherOS v0.3.1-rc2 started')  # TODO use ./scripts/version
+        print(str.strip(ln))
         if ros_booted_substr > -1:
             assert True
             return
@@ -31,14 +30,14 @@ def test_system_boot(qemu):
 @pytest.mark.timeout(40)
 def test_run_system_container(qemu):
     assert qemu.returncode is None
-    time.sleep(1)  # or else ssh will fail (WTF?!)
+    time.sleep(2)  # or else ssh will fail (WTF?!)
     ssh = subprocess.Popen(
         './scripts/ssh sudo system-docker run --rm busybox /bin/true', shell=True,
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
     try:
         for ln in iter(ssh.stdout.readline, ''):
-            print(string.strip(ln))
+            print(str.strip(ln))
             pass
         ssh.wait()
         assert ssh.returncode == 0
