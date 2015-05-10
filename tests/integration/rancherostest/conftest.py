4886c8815c38f9b7add50d573e4295b094736da9
Ivan Mikushin <i.mikushin@gmail.com>
2015-05-10 17:11:40 +0500

    make ssh happy with the key perms (again)

--- tests/integration/rancherostest/conftest.py
+++ tests/integration/rancherostest/conftest.py
@@ -6,3 +6,5 @@ import pytest
 def chdir_to_project_root():
     os.chdir('../..')
     print('\nChdir to project root dir')
+    os.chmod('./tests/integration/assets/test.key', 0o600)
+    print('Also, `chmod 600 tests/integration/assets/test.key` to make ssh happy')
