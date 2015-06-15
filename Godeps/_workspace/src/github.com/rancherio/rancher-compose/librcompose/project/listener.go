b14a50ec9d4fbd8a523cb649866c2e82336e54ef
Ivan Mikushin <i.mikushin@gmail.com>
2015-06-16 01:54:16 +0500

    update Godeps
    for latest docker and rancher-compose

---
+++ Godeps/_workspace/src/github.com/rancherio/rancher-compose/librcompose/project/listener.go
@@ -0,0 +1,66 @@
+package project
+
+import (
+	"bytes"
+
+	"github.com/Sirupsen/logrus"
+)
+
+var (
+	infoEvents = map[Event]bool{
+		PROJECT_DELETE_DONE:   true,
+		PROJECT_DELETE_START:  true,
+		PROJECT_DOWN_DONE:     true,
+		PROJECT_DOWN_START:    true,
+		PROJECT_RESTART_DONE:  true,
+		PROJECT_RESTART_START: true,
+		PROJECT_UP_DONE:       true,
+		PROJECT_UP_START:      true,
+		SERVICE_DELETE_START:  true,
+		SERVICE_DELETE:        true,
+		SERVICE_DOWN_START:    true,
+		SERVICE_DOWN:          true,
+		SERVICE_RESTART_START: true,
+		SERVICE_RESTART:       true,
+		SERVICE_UP_START:      true,
+		SERVICE_UP:            true,
+	}
+)
+
+type defaultListener struct {
+	project    *Project
+	listenChan chan ProjectEvent
+	upCount    int
+}
+
+func (d *defaultListener) start() {
+	for event := range d.listenChan {
+		buffer := bytes.NewBuffer(nil)
+		if event.Data != nil {
+			for k, v := range event.Data {
+				if buffer.Len() > 0 {
+					buffer.WriteString(", ")
+				}
+				buffer.WriteString(k)
+				buffer.WriteString("=")
+				buffer.WriteString(v)
+			}
+		}
+
+		if event.Event == SERVICE_UP {
+			d.upCount++
+		}
+
+		logf := logrus.Debugf
+
+		if infoEvents[event.Event] {
+			logf = logrus.Infof
+		}
+
+		if event.ServiceName == "" {
+			logf("Project [%s]: %s %s", d.project.Name, event.Event, buffer.Bytes())
+		} else {
+			logf("[%d/%d] [%s]: %s %s", d.upCount, len(d.project.Configs), event.ServiceName, event.Event, buffer.Bytes())
+		}
+	}
+}
--- Godeps/_workspace/src/github.com/rancherio/rancher-compose/librcompose/project/types.go
+++ Godeps/_workspace/src/github.com/rancherio/rancher-compose/librcompose/project/types.go
@@ -11,75 +11,116 @@ const (
 	CONTAINER_CREATED  = Event("Created container")
 	CONTAINER_STARTED  = Event("Started container")
 
-	SERVICE_ADD      = Event("Adding")
-	SERVICE_UP_START = Event("Starting")
-	SERVICE_UP       = Event("Started")
+	SERVICE_ADD           = Event("Adding")
+	SERVICE_UP_START      = Event("Starting")
+	SERVICE_UP_IGNORED    = Event("Ignoring")
+	SERVICE_UP            = Event("Started")
+	SERVICE_CREATE_START  = Event("Creating")
+	SERVICE_CREATE        = Event("Created")
+	SERVICE_DELETE_START  = Event("Deleting")
+	SERVICE_DELETE        = Event("Deleted")
+	SERVICE_DOWN_START    = Event("Stopping")
+	SERVICE_DOWN          = Event("Stopped")
+	SERVICE_RESTART_START = Event("Restarting")
+	SERVICE_RESTART       = Event("Restarted")
 
+	PROJECT_DOWN_START     = Event("Stopping project")
+	PROJECT_DOWN_DONE      = Event("Project stopped")
+	PROJECT_CREATE_START   = Event("Creating project")
+	PROJECT_CREATE_DONE    = Event("Project created")
 	PROJECT_UP_START       = Event("Starting project")
 	PROJECT_UP_DONE        = Event("Project started")
+	PROJECT_DELETE_START   = Event("Deleting project")
+	PROJECT_DELETE_DONE    = Event("Project deleted")
+	PROJECT_RESTART_START  = Event("Restarting project")
+	PROJECT_RESTART_DONE   = Event("Project restarted")
 	PROJECT_RELOAD         = Event("Reloading project")
 	PROJECT_RELOAD_TRIGGER = Event("Triggering project reload")
 )
 
 type ServiceConfig struct {
-	CapAdd      []string      `yaml:"cap_add,omitempty"`
-	CapDrop     []string      `yaml:"cap_drop,omitempty"`
-	CpuShares   int64         `yaml:"cpu_shares,omitempty"`
-	Command     string        `yaml:"command,omitempty"`
-	Detach      string        `yaml:"detach,omitempty"`
-	Dns         Stringorslice `yaml:"dns,omitempty"`
-	DnsSearch   Stringorslice `yaml:"dns_search,omitempty"`
-	DomainName  string        `yaml:"domainname,omitempty"`
-	Entrypoint  string        `yaml:"entrypoint,omitempty"`
-	EnvFile     string        `yaml:"env_file,omitempty"`
-	Environment Maporslice    `yaml:"environment,omitempty"`
-	Hostname    string        `yaml:"hostname,omitempty"`
-	Image       string        `yaml:"image,omitempty"`
-	Labels      SliceorMap    `yaml:"labels,omitempty"`
-	Links       []string      `yaml:"links,omitempty"`
-	LogDriver   string        `yaml:"log_driver,omitempty"`
-	MemLimit    int64         `yaml:"mem_limit,omitempty"`
-	Name        string        `yaml:"name,omitempty"`
-	Net         string        `yaml:"net,omitempty"`
-	Pid         string        `yaml:"pid,omitempty"`
-	Ipc         string        `yaml:"ipc,omitempty"`
-	Ports       []string      `yaml:"ports,omitempty"`
-	Privileged  bool          `yaml:"privileged,omitempty"`
-	Restart     string        `yaml:"restart,omitempty"`
-	ReadOnly    bool          `yaml:"read_only,omitempty"`
-	StdinOpen   bool          `yaml:"stdin_open,omitempty"`
-	Tty         bool          `yaml:"tty,omitempty"`
-	User        string        `yaml:"user,omitempty"`
-	Volumes     []string      `yaml:"volumes,omitempty"`
-	VolumesFrom []string      `yaml:"volumes_from,omitempty"`
-	WorkingDir  string        `yaml:"working_dir,omitempty"`
-	//`yaml:"build,omitempty"`
-	Expose        []string `yaml:"expose,omitempty"`
-	ExternalLinks []string `yaml:"external_links,omitempty"`
+	Build         string            `yaml:"build,omitempty"`
+	CapAdd        []string          `yaml:"cap_add,omitempty"`
+	CapDrop       []string          `yaml:"cap_drop,omitempty"`
+	CpuSet        string            `yaml:"cpu_set,omitempty"`
+	CpuShares     int64             `yaml:"cpu_shares,omitempty"`
+	Command       Command           `yaml:"command"` // omitempty breaks serialization!
+	Detach        string            `yaml:"detach,omitempty"`
+	Devices       []string          `yaml:"devices,omitempty"`
+	Dns           Stringorslice     `yaml:"dns"`        // omitempty breaks serialization!
+	DnsSearch     Stringorslice     `yaml:"dns_search"` // omitempty breaks serialization!
+	Dockerfile    string            `yaml:"dockerfile,omitempty"`
+	DomainName    string            `yaml:"domainname,omitempty"`
+	Entrypoint    Command           `yaml:"entrypoint"`  // omitempty breaks serialization!
+	EnvFile       Stringorslice     `yaml:"env_file"`    // omitempty breaks serialization!
+	Environment   MaporEqualSlice   `yaml:"environment"` // omitempty breaks serialization!
+	Hostname      string            `yaml:"hostname,omitempty"`
+	Image         string            `yaml:"image,omitempty"`
+	Labels        SliceorMap        `yaml:"labels"` // omitempty breaks serialization!
+	Links         MaporColonSlice   `yaml:"links"`  // omitempty breaks serialization!
+	LogDriver     string            `yaml:"log_driver,omitempty"`
+	MemLimit      int64             `yaml:"mem_limit,omitempty"`
+	MemSwapLimit  int64             `yaml:"mem_swap_limit,omitempty"`
+	Name          string            `yaml:"name,omitempty"`
+	Net           string            `yaml:"net,omitempty"`
+	Pid           string            `yaml:"pid,omitempty"`
+	Uts           string            `yaml:"uts,omitempty"`
+	Ipc           string            `yaml:"ipc,omitempty"`
+	Ports         []string          `yaml:"ports,omitempty"`
+	Privileged    bool              `yaml:"privileged,omitempty"`
+	Restart       string            `yaml:"restart,omitempty"`
+	ReadOnly      bool              `yaml:"read_only,omitempty"`
+	StdinOpen     bool              `yaml:"stdin_open,omitempty"`
+	SecurityOpt   []string          `yaml:"security_opt,omitempty"`
+	Tty           bool              `yaml:"tty,omitempty"`
+	User          string            `yaml:"user,omitempty"`
+	Volumes       []string          `yaml:"volumes,omitempty"`
+	VolumesFrom   []string          `yaml:"volumes_from,omitempty"`
+	WorkingDir    string            `yaml:"working_dir,omitempty"`
+	Expose        []string          `yaml:"expose,omitempty"`
+	ExternalLinks []string          `yaml:"external_links,omitempty"`
+	LogOpt        map[string]string `yaml:"log_opt,omitempty"`
+	ExtraHosts    []string          `yaml:"extra_hosts,omitempty"`
 }
 
 type EnvironmentLookup interface {
 	Lookup(key, serviceName string, config *ServiceConfig) []string
 }
 
+type ConfigLookup interface {
+	Lookup(file, relativeTo string) ([]byte, string, error)
+}
+
 type Project struct {
 	EnvironmentLookup EnvironmentLookup
+	ConfigLookup      ConfigLookup
 	Name              string
-	configs           map[string]*ServiceConfig
+	Configs           map[string]*ServiceConfig
 	reload            []string
-	file              string
-	content           []byte
+	File              string
 	client            *client.RancherClient
 	factory           ServiceFactory
 	ReloadCallback    func() error
 	upCount           int
 	listeners         []chan<- ProjectEvent
+	hasListeners      bool
+}
+
+type Container struct {
+	Name  string
+	State string
 }
 
 type Service interface {
 	Name() string
+	Create() error
 	Up() error
+	Down() error
+	Delete() error
+	Restart() error
+	Log() error
 	Config() *ServiceConfig
+	Scale(count int) error
 }
 
 type ServiceFactory interface {
