ee257f944ee1e5c54503081b114c8897aa6215a8
Darren Shepherd <darren@rancher.com>
2015-12-22 17:11:25 -0700

    Remove unneeded code

--- init/one.go
+++ init/one.go
@@ -12,13 +12,9 @@ func pidOne() error {
 	c := make(chan os.Signal)
 	signal.Notify(c, syscall.SIGCHLD)
 
-	var (
-		ws  syscall.WaitStatus
-		rus syscall.Rusage
-	)
 	for range c {
 		for {
-			if pid, err := syscall.Wait4(-1, &ws, syscall.WNOHANG, &rus); err != nil || pid <= 0 {
+			if pid, err := syscall.Wait4(-1, nil, syscall.WNOHANG, nil); err != nil || pid <= 0 {
 				break
 			}
 		}
