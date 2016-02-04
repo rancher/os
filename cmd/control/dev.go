28bd8b0ccb58318734a59632fde2946abc38b8fa
Darren Shepherd <darren@rancher.com>
2016-02-04 09:25:36 -0700

    Fix ros dev panic

--- cmd/control/dev.go
+++ cmd/control/dev.go
@@ -2,10 +2,13 @@ package control
 
 import (
 	"fmt"
+
 	"github.com/codegangsta/cli"
 	"github.com/rancher/os/util"
 )
 
 func devAction(c *cli.Context) {
-	fmt.Println(util.ResolveDevice(c.Args()[0]))
+	if len(c.Args()) > 0 {
+		fmt.Println(util.ResolveDevice(c.Args()[0]))
+	}
 }
--- tests/integration/rostest/test_00_system.py
+++ tests/integration/rostest/test_00_system.py
@@ -31,3 +31,18 @@ def test_run_system_container(qemu):
     ssh.wait()
 
     assert ssh.returncode == 0
+
+
+@pytest.mark.timeout(60)
+def test_ros_dev(qemu):
+    u.wait_for_ssh(qemu)
+
+    ssh = subprocess.Popen(
+        './scripts/ssh --qemu sudo ros dev',
+        shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+
+    for ln in u.iter_lines(ssh.stdout):
+        print(ln)
+    ssh.wait()
+
+    assert ssh.returncode == 0
