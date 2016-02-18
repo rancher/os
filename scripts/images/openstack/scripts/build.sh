01252fc044353e1d9758b57a07dafa2b36de1478
Darren Shepherd <darren@rancher.com>
2016-02-18 10:10:56 -0700

    Scripts to build OpenStack/KVM image

---
+++ scripts/images/openstack/scripts/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)/..
+
+mkdir -p {dist,build/openstack/latest}
+
+cat > build/openstack/latest/user_data << EOF
+#!/bin/bash
+set -e
+
+trap "poweroff" EXIT
+
+mount -t 9p -o trans=virtio,version=9p2000.L config-2 /mnt
+
+touch log
+openvt -s -- tail -f log &
+ros install -d /dev/vda -f --no-reboot >log 2>&1
+
+touch /mnt/success
+EOF
+
+rm -f build/{success,hd.img}
+qemu-img create -f qcow2 build/hd.img 8G
+kvm -curses \
+    -drive if=virtio,file=build/hd.img \
+    -cdrom assets/rancheros.iso \
+    -m 1024 \
+    -fsdev local,id=conf,security_model=none,path=$(pwd)/build \
+    -device virtio-9p-pci,fsdev=conf,mount_tag=config-2
+
+[ -f build/success ]
+
+echo Converting dist/rancheros-openstack.img
+qemu-img convert -c -O qcow2 build/hd.img dist/rancheros-openstack.img
