f28d463504ede4d2f647deb9737b66cf67d4a86e
Josh Curl <hello@joshcurl.com>
2016-02-19 16:11:32 -0800

    Enable SELinux

---
+++ selinux/selinux.go
@@ -0,0 +1,13 @@
+// +build linux
+
+package selinux
+
+// #cgo pkg-config: libselinux libsepol
+// #include <selinux/selinux.h>
+import "C"
+
+func InitializeSelinux() (int, error) {
+	enforce := C.int(0)
+	ret, err := C.selinux_init_load_policy(&enforce)
+	return int(ret), err
+}
