4439cdde4198cc60447d7d8a4fc660cc9076f9a4
Wang Long <long.wanglong@huawei.com>
2016-04-19 11:30:30 +0800

    Modify the test_11_environment to work with multiple architectures
    Signed-off-by: Wang Long <long.wanglong@huawei.com>

--- tests/integration/rostest/test_11_environment.py
+++ tests/integration/rostest/test_11_environment.py
@@ -3,12 +3,14 @@ import rostest.util as u
 from rostest.util import SSH
 
 ssh_command = ['./scripts/ssh', '--qemu', '--key', './tests/integration/assets/test.key']
-cloud_config_path = './tests/integration/assets/test_11/cloud-config.yml'
+cloud_config_path = {'amd64': './tests/integration/assets/test_11/cloud-config.yml',
+                     'arm': './tests/integration/assets/test_11/cloud-config_arm.yml',
+                     'arm64': './tests/integration/assets/test_11/cloud-config_arm64.yml'}
 
 
 @pytest.fixture(scope="module")
 def qemu(request):
-    q = u.run_qemu(request, ['--cloud-config', cloud_config_path])
+    q = u.run_qemu(request, ['--cloud-config', cloud_config_path[u.arch]])
     u.flush_out(q.stdout)
     return q
 
