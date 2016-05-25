69fe4bb61917e35ee6335dc6059171b6d2eff084
Josh Curl <josh@curl.me>
2016-05-25 09:40:28 -0700

    Cache remote repos and service definitions

---
+++ util/network/network_test.go
@@ -0,0 +1,23 @@
+package network
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func NoTestLoadResourceSimple(t *testing.T) {
+	assert := require.New(t)
+
+	expected := `services:
+- debian-console
+- ubuntu-console
+`
+	expected = strings.TrimSpace(expected)
+
+	b, e := LoadResource("https://raw.githubusercontent.com/rancher/os-services/v0.3.4/index.yml", true)
+
+	assert.Nil(e)
+	assert.Equal(expected, strings.TrimSpace(string(b)))
+}
