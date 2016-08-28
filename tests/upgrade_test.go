8777e477b821612dcc22eaef969b2c893510e741
Josh Curl <josh@curl.me>
2016-08-28 08:58:17 -0700

    Add upgrade tests

---
+++ tests/upgrade_test.go
@@ -0,0 +1,26 @@
+package integration
+
+import (
+	"fmt"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *QemuSuite) TestUpgrade(c *C) {
+	err := s.RunQemuInstalled()
+	c.Assert(err, IsNil)
+
+	s.CheckCall(c, `
+set -ex
+sudo ros os upgrade -i rancher/os:v0.5.0 --force --no-reboot`)
+
+	s.Reboot(c)
+
+	s.CheckCall(c, "sudo ros -v | grep v0.5.0")
+	s.LoadInstallerImage(c)
+	s.CheckCall(c, fmt.Sprintf("sudo ros os upgrade -i rancher/os:%s%s --force --no-reboot", Version, Suffix))
+
+	s.Reboot(c)
+
+	s.CheckCall(c, fmt.Sprintf("sudo ros -v | grep %s", Version))
+}
