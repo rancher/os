ff98f274074ab01b2b59257a73e19624cc4afe1e
Sven Dowideit <SvenDowideit@home.org.au>
2017-03-06 15:02:24 +1000

    use a smaller console to test
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- tests/network_on_boot_test.go
+++ tests/network_on_boot_test.go
@@ -5,6 +5,6 @@ import . "gopkg.in/check.v1"
 func (s *QemuSuite) TestNetworkOnBoot(c *C) {
 	s.RunQemu(c, "--cloud-config", "./tests/assets/test_18/cloud-config.yml", "-net", "nic,vlan=1,model=virtio")
 
-	s.CheckCall(c, "apt-get --version")
+	s.CheckCall(c, "apk --version")
 	s.CheckCall(c, "sudo system-docker images | grep tianon/true")
 }
