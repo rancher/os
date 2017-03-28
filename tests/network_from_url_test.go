8327006f6135e65efeba3a56a7ae24818ab102fb
Sven Dowideit <SvenDowideit@home.org.au>
2017-03-28 21:18:13 +1000

    Failing tests for long timeouts when there is no network
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- tests/network_from_url_test.go
+++ tests/network_from_url_test.go
@@ -8,9 +8,9 @@ func (s *QemuSuite) TestNetworkFromUrl(c *C) {
 	for i := 0; i < 7; i++ {
 		args = append(args, netArgs...)
 	}
-	s.RunQemu(c, args...)
+	s.RunQemuWithNetConsole(c, args...)
 
-	s.CheckCall(c, `
+	s.NetCheckCall(c, `
 cat > test-merge << "SCRIPT"
 set -x -e
 
@@ -21,7 +21,7 @@ ip link show dev eth1.100 | grep 'master br0'
 SCRIPT
 sudo bash test-merge`)
 
-	s.CheckCall(c, `
+	s.NetCheckCall(c, `
 cat > test-merge << "SCRIPT"
 set -x -e
 
@@ -31,4 +31,26 @@ cat /etc/resolv.conf | grep "nameserver 208.67.220.123"
 
 SCRIPT
 sudo bash test-merge`)
+}
+
+func (s *QemuSuite) TestNoNetworkCloudConfigFromUrl(c *C) {
+	args := []string{
+		"--no-network",
+		"--append",
+		"rancher.debug=true rancher.password=test-me rancher.cloud_init.datasources=[url:https://gist.githubusercontent.com/joshwget/0bdc616cd26162ad87c535644c8b1ef6/raw/8cce947c08cf006e932b71d92ddbb96bae8e3325/gistfile1.txt]",
+	}
+	s.RunQemuWithNetConsole(c, args...)
+
+	s.NetCheckCall(c, "sudo ros config get rancher.log | grep true")
+}
+
+func (s *QemuSuite) TestNoNetworkConsoleSwitch(c *C) {
+	args := []string{
+		"--no-network",
+		"--append",
+		"rancher.debug=true rancher.password=test-me rancher.console=alpine",
+	}
+	s.RunQemuWithNetConsole(c, args...)
+
+	s.NetCheckCall(c, "uname -a")
 }
