9d3741e9cc78c4915da44194c522bb15db015da4
Sven Dowideit <SvenDowideit@home.org.au>
2017-05-09 21:08:23 +1000

    Not entirely sure why this test passes
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- tests/boot2docker_autoformat_test.go
+++ tests/boot2docker_autoformat_test.go
@@ -6,14 +6,14 @@ import . "gopkg.in/check.v1"
 
 func (s *QemuSuite) TestBoot2DockerState(c *C) {
 	s.RunQemu(c, "--fresh", "--b2d")
-	s.CheckCall(c, "blkid | grep B2D_STATE")
+	s.CheckCall(c, "sudo blkid | grep B2D_STATE")
 	// And once I make run create a tar file, check that its untarred in the docker user's home dir
 	// And confirm if it should add to the dir, or replace, i can't remember
 }
 
 func (s *QemuSuite) TestIsoBoot2DockerState(c *C) {
 	s.RunQemu(c, "--fresh", "--b2d", "--iso")
-	s.CheckCall(c, "blkid | grep B2D_STATE")
+	s.CheckCall(c, "sudo blkid | grep B2D_STATE")
 	s.CheckCall(c, "sudo ros config get rancher.state.dev | grep LABEL=B2D_STATE")
 	s.CheckCall(c, "sudo ros config get rancher.state.autoformat | grep vda")
 	// And once I make run create a tar file, check that its untarred in the docker user's home dir
@@ -22,5 +22,5 @@ func (s *QemuSuite) TestIsoBoot2DockerState(c *C) {
 
 func (s *QemuSuite) TestRancherOSState(c *C) {
 	s.RunQemu(c, "--fresh")
-	s.CheckCall(c, "blkid | grep RANCHER_STATE")
+	s.CheckCall(c, "sudo blkid | grep RANCHER_STATE")
 }
