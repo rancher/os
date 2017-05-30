69c806ca057be9e7c1c48ca1b4cdd023e73fea5d
Adam Pointer <adam.pointer@skybettingandgaming.com>
2017-05-30 13:40:36 +0100

    Add audit rules plus kill switch to cloud config

--- init/auditd.go
+++ init/auditd.go
@@ -3,29 +3,49 @@
 package init
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"strings"
 
 	"github.com/rancher/os/config"
 	"github.com/rancher/os/log"
 )
 
+const rulesFile = "/etc/audit/audit.rules"
+
 func initializeAuditd(c *config.CloudConfig) (*config.CloudConfig, error) {
-	for _, d := range []string{"/var/log/audit"} {
-		if err := os.MkdirAll(d, os.ModeDir); err != nil {
-			log.Debug(err)
-			return c, err
+	if c.Rancher.Auditd.Enabled {
+		for _, d := range []string{"/var/log/audit", path.Dir(rulesFile)} {
+			if err := os.MkdirAll(d, os.ModeDir); err != nil {
+				log.Debug(err)
+				return c, err
+			}
 		}
-	}
-	cmds := []*exec.Cmd{
-		exec.Command("auditctl", "-R", "/etc/audit/audit.rules"),
-		exec.Command("auditd", "-n"),
-	}
-	for _, cmd := range cmds {
-		if err := cmd.Start(); err != nil {
-			log.Debug(err)
-			return c, err
+		if err := writeRules(&c.Rancher.Auditd); err != nil {
+			return c, fmt.Errorf("Error writing audit rules: %s", err)
+		}
+		cmds := []*exec.Cmd{
+			exec.Command("auditctl", "-R", rulesFile),
+			exec.Command("auditd", "-n"),
+		}
+		for _, cmd := range cmds {
+			if err := cmd.Start(); err != nil {
+				log.Debug(err)
+				return c, err
+			}
 		}
 	}
 	return c, nil
+}
+
+func writeRules(r *config.AuditConfig) error {
+	rules := strings.Join(r.Rules, "\n")
+
+	if err := ioutil.WriteFile(rulesFile, []byte(rules), 0400); err != nil {
+		return err
+	}
+	return nil
 }
