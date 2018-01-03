rancher/os

commit 51aff79c7e04d4ed1d81ceb71a17263d5c7c8eab

Add proper vmware cloud-init datasource from guestinfo
Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

init/init.go
@@ -300,6 +300,11 @@ func RunInit() error {
 		}},
 		config.CfgFuncData{"cloud-init", func(cfg *config.CloudConfig) (*config.CloudConfig, error) {
 			cfg.Rancher.CloudInit.Datasources = config.LoadConfigWithPrefix(state).Rancher.CloudInit.Datasources
+			hypervisor := checkHypervisor(cfg)
+			if hypervisor == "vmware" {
+				// add vmware to the end - we don't want to over-ride an choices the user has made
+				cfg.Rancher.CloudInit.Datasources = append(cfg.Rancher.CloudInit.Datasources, hypervisor)
+			}
 			if err := config.Set("rancher.cloud_init.datasources", cfg.Rancher.CloudInit.Datasources); err != nil {
 				log.Error(err)
 			}
@@ -362,10 +367,6 @@ func RunInit() error {
 
 			return cfg, nil
 		}},
-		config.CfgFuncData{"detect hypervisor", func(cfg *config.CloudConfig) (*config.CloudConfig, error) {
-			checkHypervisor()
-			return config.LoadConfig(), nil
-		}},
 		config.CfgFuncData{"b2d Env", func(cfg *config.CloudConfig) (*config.CloudConfig, error) {
 
 			if boot2DockerEnvironment {
@@ -409,7 +410,7 @@ func RunInit() error {
 	return pidOne()
 }
 
-func checkHypervisor() {
+func checkHypervisor(cfg *config.CloudConfig) string {
 	hvtools := cpuid.CPU.HypervisorName
 	if hvtools != "" {
 		log.Infof("Detected Hypervisor: %s", cpuid.CPU.HypervisorName)
@@ -421,4 +422,5 @@ func checkHypervisor() {
 			log.Error(err)
 		}
 	}
+	return cpuid.CPU.HypervisorName
 }

cmd/cloudinitsave/cloudinitsave.go
@@ -39,6 +39,7 @@ import (
 	"github.com/rancher/os/config/cloudinit/datasource/metadata/packet"
 	"github.com/rancher/os/config/cloudinit/datasource/proccmdline"
 	"github.com/rancher/os/config/cloudinit/datasource/url"
+	"github.com/rancher/os/config/cloudinit/datasource/vmware"
 	"github.com/rancher/os/config/cloudinit/pkg"
 	"github.com/rancher/os/log"
 	"github.com/rancher/os/netconf"
@@ -229,7 +230,7 @@ func getDatasources(datasources []string) []datasource.Datasource {
 
 		switch parts[0] {
 		case "*":
-			dss = append(dss, getDatasources([]string{"configdrive", "ec2", "digitalocean", "packet", "gce"})...)
+			dss = append(dss, getDatasources([]string{"configdrive", "vmware", "ec2", "digitalocean", "packet", "gce"})...)
 		case "ec2":
 			dss = append(dss, ec2.NewDatasource(root))
 		case "file":
@@ -256,6 +257,8 @@ func getDatasources(datasources []string) []datasource.Datasource {
 			dss = append(dss, gce.NewDatasource(root))
 		case "packet":
 			dss = append(dss, packet.NewDatasource(root))
+		case "vmware":
+			dss = append(dss, vmware.NewDatasource(root))
 		}
 	}
 

config/cloudinit/datasource/vmware/vmware.go
@@ -131,19 +131,19 @@ func (v VMWare) FetchMetadata() (metadata datasource.Metadata, err error) {
 }
 
 func (v VMWare) FetchUserdata() ([]byte, error) {
-	encoding, err := v.readConfig("coreos.config.data.encoding")
+	encoding, err := v.readConfig("cloud-init.data.encoding")
 	if err != nil {
 		return nil, err
 	}
 
-	data, err := v.readConfig("coreos.config.data")
+	data, err := v.readConfig("cloud-init.config.data")
 	if err != nil {
 		return nil, err
 	}
 
 	// Try to fallback to url if no explicit data
 	if data == "" {
-		url, err := v.readConfig("coreos.config.url")
+		url, err := v.readConfig("cloud-init.config.url")
 		if err != nil {
 			return nil, err
 		}

commit 69378127b089c51fc0d0c03549f0c73005cf3eb2

Fixed regression: User-configured datasources ignored in cloud-init-save
Bug introduced in https://github.com/rancher/os/commit/51aff79c7e04d4ed1d81ceb71a17263d5c7c8eab#diff-add8541b8b3d498a5afdf604303ea0d4

Signed-off-by: janeczku <jan@rancher.com>

init/init.go
@@ -334,9 +334,10 @@ func RunInit() error {
 			if hypervisor == "vmware" {
 				// add vmware to the end - we don't want to over-ride an choices the user has made
 				cfg.Rancher.CloudInit.Datasources = append(cfg.Rancher.CloudInit.Datasources, hypervisor)
-				if err := config.Set("rancher.cloud_init.datasources", cfg.Rancher.CloudInit.Datasources); err != nil {
-					log.Error(err)
-				}
+			}
+
+			if err := config.Set("rancher.cloud_init.datasources", cfg.Rancher.CloudInit.Datasources); err != nil {
+				log.Error(err)
 			}
 
 			log.Infof("init, runCloudInitServices(%v)", cfg.Rancher.CloudInit.Datasources)