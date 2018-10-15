ab4e0e590bed445d0c779af75c29470e564a5c78
niusmallnan <niusmallnan@gmail.com>
2018-10-15 16:36:52 +0800

    Fix a bug when installing ros to disk with noformat.
    The cloud-config files cannot written to RANCHER_STATE disk when users
    set the RANCHER_BOOT partition.

--- cmd/control/install/install.go
+++ cmd/control/install/install.go
@@ -44,7 +44,6 @@ func MountDevice(baseName, device, partition string, raw bool) (string, string, 
 		//rootfs := partition
 		// Don't use ResolveDevice - it can fail, whereas `blkid -L LABEL` works more often
 
-		cfg := config.LoadConfig()
 		d, _, err := util.Blkid("RANCHER_BOOT")
 		if err != nil {
 			log.Errorf("Failed to run blkid: %s", err)
@@ -53,18 +52,7 @@ func MountDevice(baseName, device, partition string, raw bool) (string, string, 
 			partition = d
 			baseName = filepath.Join(baseName, config.BootDir)
 		} else {
-			if dev := util.ResolveDevice(cfg.Rancher.State.Dev); dev != "" {
-				// try the rancher.state.dev setting
-				partition = dev
-			} else {
-				d, _, err := util.Blkid("RANCHER_STATE")
-				if err != nil {
-					log.Errorf("Failed to run blkid: %s", err)
-				}
-				if d != "" {
-					partition = d
-				}
-			}
+			partition = GetStatePartition()
 		}
 		cmd := exec.Command("lsblk", "-no", "pkname", partition)
 		log.Debugf("Run(%v)", cmd)
@@ -79,4 +67,18 @@ func MountDevice(baseName, device, partition string, raw bool) (string, string, 
 	//cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 	log.Debugf("mountdevice return2 -> d: %s, p: %s", device, partition)
 	return device, partition, cmd.Run()
+}
+
+func GetStatePartition() string {
+	cfg := config.LoadConfig()
+
+	if dev := util.ResolveDevice(cfg.Rancher.State.Dev); dev != "" {
+		// try the rancher.state.dev setting
+		return dev
+	}
+	d, _, err := util.Blkid("RANCHER_STATE")
+	if err != nil {
+		log.Errorf("Failed to run blkid: %s", err)
+	}
+	return d
 }
