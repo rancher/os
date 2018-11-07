4cb0f462bddf2d6345d6b06ac87ce30437788aa5
Jason-ZW <zhenyang@rancher.com>
2018-11-07 17:04:45 +0800

    Add wifi support

---
+++ images/01-base/templates/dhcpcd.conf.tpl
@@ -0,0 +1,46 @@
+# A sample configuration for dhcpcd.
+# See dhcpcd.conf(5) for details.
+
+# Allow users of this group to interact with dhcpcd via the control socket.
+#controlgroup wheel
+
+# Inform the DHCP server of our hostname for DDNS.
+hostname
+
+# Use the hardware address of the interface for the Client ID.
+#clientid
+# or
+# Use the same DUID + IAID as set in DHCPv6 for DHCPv4 ClientID as per RFC4361.
+# Some non-RFC compliant DHCP servers do not reply with this set.
+# In this case, comment out duid and enable clientid above.
+clientid
+
+# Persist interface configuration when dhcpcd exits.
+persistent
+
+# Rapid commit support.
+# Safe to enable by default because it requires the equivalent option set
+# on the server to actually work.
+option rapid_commit
+
+# A list of options to request from the DHCP server.
+option domain_name_servers, domain_name, domain_search, host_name
+option classless_static_routes
+# Most distributions have NTP support.
+option ntp_servers
+# Respect the network MTU. This is applied to DHCP routes.
+option interface_mtu
+
+# A ServerID is required by RFC2131.
+require dhcp_server_identifier
+
+# Generate SLAAC address using the Hardware Address of the interface
+#slaac hwaddr
+# OR generate Stable Private IPv6 Addresses based from the DUID
+slaac private
+
+{{- range $key, $value := .}}
+interface {{$key}}
+static ip_address={{$value.Address}}
+static routers={{$value.Gateway}}
+{{- end}}
--- pkg/netconf/types.go
+++ pkg/netconf/types.go
@@ -1,14 +1,15 @@
 package netconf
 
 type NetworkConfig struct {
-	PreCmds     []string                   `yaml:"pre_cmds,omitempty"`
-	DHCPTimeout int                        `yaml:"dhcp_timeout,omitempty"`
-	DNS         DNSConfig                  `yaml:"dns,omitempty"`
-	Interfaces  map[string]InterfaceConfig `yaml:"interfaces,omitempty"`
-	PostCmds    []string                   `yaml:"post_cmds,omitempty"`
-	HTTPProxy   string                     `yaml:"http_proxy,omitempty"`
-	HTTPSProxy  string                     `yaml:"https_proxy,omitempty"`
-	NoProxy     string                     `yaml:"no_proxy,omitempty"`
+	PreCmds      []string                     `yaml:"pre_cmds,omitempty"`
+	DHCPTimeout  int                          `yaml:"dhcp_timeout,omitempty"`
+	DNS          DNSConfig                    `yaml:"dns,omitempty"`
+	Interfaces   map[string]InterfaceConfig   `yaml:"interfaces,omitempty"`
+	PostCmds     []string                     `yaml:"post_cmds,omitempty"`
+	HTTPProxy    string                       `yaml:"http_proxy,omitempty"`
+	HTTPSProxy   string                       `yaml:"https_proxy,omitempty"`
+	NoProxy      string                       `yaml:"no_proxy,omitempty"`
+	WifiNetworks map[string]WifiNetworkConfig `yaml:"wifi_networks,omitempty"`
 }
 
 type InterfaceConfig struct {
@@ -27,9 +28,32 @@ type InterfaceConfig struct {
 	PostUp      []string          `yaml:"post_up,omitempty"`
 	PreUp       []string          `yaml:"pre_up,omitempty"`
 	Vlans       string            `yaml:"vlans,omitempty"`
+	WifiNetwork string            `yaml:"wifi_network,omitempty"`
 }
 
 type DNSConfig struct {
 	Nameservers []string `yaml:"nameservers,flow,omitempty"`
 	Search      []string `yaml:"search,flow,omitempty"`
+}
+
+type WifiNetworkConfig struct {
+	Address           string   `yaml:"address,omitempty"`
+	Gateway           string   `yaml:"gateway,omitempty"`
+	ScanSSID          int      `yaml:"scan_ssid,omitempty"`
+	SSID              string   `yaml:"ssid,omitempty"`
+	PSK               string   `yaml:"psk,omitempty"`
+	Priority          int      `yaml:"priority,omitempty"`
+	Pairwise          string   `yaml:"pairwise,omitempty"`
+	Group             string   `yaml:"group,omitempty"`
+	Eap               string   `yaml:"eap,omitempty"`
+	Identity          string   `yaml:"identity,omitempty"`
+	AnonymousIdentity string   `yaml:"anonymous_identity,omitempty"`
+	CaCerts           []string `yaml:"ca_certs,omitempty"`
+	ClientCerts       []string `yaml:"client_certs,omitempty"`
+	PrivateKeys       []string `yaml:"private_keys,omitempty"`
+	PrivateKeyPasswds []string `yaml:"private_key_passwds,omitempty"`
+	Phases            []string `yaml:"phases,omitempty"`
+	EapolFlags        int      `yaml:"eapol_flags,omitempty"`
+	KeyMgmt           string   `yaml:"key_mgmt,omitempty"`
+	Password          string   `yaml:"password,omitempty"`
 }
