e586df355763158d364efd97503f8cfd0b443248
Jason-ZW <zhenyang@rancher.com>
2019-01-04 15:22:32 +0800

    Add docker-18.09.0

--- cmd/control/util.go
+++ cmd/control/util.go
@@ -28,3 +28,30 @@ func formatImage(image string, cfg *config.CloudConfig) string {
 	}
 	return image
 }
+
+func symLinkEngineBinary(version string) []symlink {
+	baseSymlink := []symlink{
+		{"/var/lib/rancher/engine/docker", "/usr/bin/docker"},
+		{"/var/lib/rancher/engine/dockerd", "/usr/bin/dockerd"},
+		{"/var/lib/rancher/engine/docker-init", "/usr/bin/docker-init"},
+		{"/var/lib/rancher/engine/docker-proxy", "/usr/bin/docker-proxy"},
+		{"/usr/share/ros/os-release", "/usr/lib/os-release"},
+		{"/usr/share/ros/os-release", "/etc/os-release"},
+	}
+	if strings.Contains(version, "18.09") {
+		baseSymlink = append(baseSymlink, []symlink{
+			{"/var/lib/rancher/engine/containerd", "/usr/bin/containerd"},
+			{"/var/lib/rancher/engine/ctr", "/usr/bin/ctr"},
+			{"/var/lib/rancher/engine/containerd-shim", "/usr/bin/containerd-shim"},
+			{"/var/lib/rancher/engine/runc", "/usr/bin/runc"},
+		}...)
+	} else {
+		baseSymlink = append(baseSymlink, []symlink{
+			{"/var/lib/rancher/engine/docker-containerd", "/usr/bin/docker-containerd"},
+			{"/var/lib/rancher/engine/docker-containerd-ctr", "/usr/bin/docker-containerd-ctr"},
+			{"/var/lib/rancher/engine/docker-containerd-shim", "/usr/bin/docker-containerd-shim"},
+			{"/var/lib/rancher/engine/docker-runc", "/usr/bin/docker-runc"},
+		}...)
+	}
+	return baseSymlink
+}
