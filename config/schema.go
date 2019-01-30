edf913012d6b578cecc193a39d5657bd810d29b8
Jason-ZW <zhenyang@rancher.com>
2019-01-30 13:32:08 +0800

    Embed subscriber service

--- config/schema.go
+++ config/schema.go
@@ -101,7 +101,8 @@ var schema = `{
 			"properties": {
 				"url": {"type": "string"},
 				"image": {"type": "string"},
-				"rollback": {"type": "string"}
+				"rollback": {"type": "string"},
+				"policy": {"type": "string"}
 			}
 		},
 
