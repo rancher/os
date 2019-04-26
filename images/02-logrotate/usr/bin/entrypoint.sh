e4e208d3b72df83138fad96c9e0edec21c923fdf
niusmallnan <niusmallnan@gmail.com>
2019-04-26 21:11:34 +0800

    Only make an initial copy for the logrotate config files

--- images/02-logrotate/usr/bin/entrypoint.sh
+++ images/02-logrotate/usr/bin/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cp /usr/share/logrotate/logrotate.d/* /etc/logrotate.d
+for f in /usr/share/logrotate/logrotate.d/*; do
+    target=/etc/logrotate.d/$(basename ${f})
+    if [ ! -e ${target} ]; then
+        cp ${f} ${target}
+    fi
+done
 
 exec /usr/bin/ros entrypoint "$@"
