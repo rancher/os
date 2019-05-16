d07d1282122975ad001d1214e686bea8e900c9c3
niusmallnan <niusmallnan@gmail.com>
2019-05-16 09:36:47 +0000

    Update vendor code

--- vendor/github.com/docker/containerd/specs/spec_linux.go
+++
@@ -1,9 +0,0 @@
-package specs
-
-import ocs "github.com/opencontainers/runtime-spec/specs-go"
-
-type (
-	ProcessSpec ocs.Process
-	Spec        ocs.Spec
-	Rlimit      ocs.Rlimit
-)
--- vendor/github.com/opencontainers/runc/main_unix.go
+++
@@ -1,5 +0,0 @@
-// +build linux
-
-package runc
-
-import _ "github.com/opencontainers/runc/libcontainer/nsenter"
