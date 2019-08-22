7cad7274136fdfa5eff277859340980d552a4f49
niusmallnan <niusmallnan@gmail.com>
2019-08-22 13:54:19 +0800

    Support for flushing cert to initrd only

--- scripts/tools/flush_crt_iso.sh
+++ scripts/tools/flush_crt_iso.sh
@@ -5,7 +5,11 @@
 #   cd scripts/tools/
 #   wget https://link/rancheros-xxx.iso
 #   wget http://link/custom.crt
+#
 #   ./flush_crt_iso.sh --iso rancheros-vmware-autoformat.iso --cert custom.crt
+#   # or
+#   ./flush_crt_iso.sh --initrd initrd-xxxx --cert custom.crt
+#
 #   exit
 #   ls ./build/
 #
@@ -21,6 +25,10 @@ mkdir -p ${ORIGIN_DIR} ${NEW_DIR} ${WORK_DIR} ${DAPPER_SOURCE}/build
 
 while [ "$#" -gt 0 ]; do
     case $1 in
+        --initrd)
+            shift 1
+            INITRD_FILE=$(readlink -f $1)
+            ;;
         --iso)
             shift 1
             ISO_FILE=$(readlink -f $1)
@@ -36,6 +44,30 @@ while [ "$#" -gt 0 ]; do
     shift 1
 done
 
+function rebuild_initrd() {
+    local initrd_name=$1
+    local output_dir=$2
+
+    # update and rebuild the initrd
+    pushd ${WORK_DIR}
+    mv initrd-* ${initrd_name}.gz
+    gzip -d ${initrd_name}.gz
+    cpio -i -F ${initrd_name}
+    rm -f ${initrd_name}
+
+    cat ${CERT_FILE} >> ${WORK_DIR}/usr/etc/ssl/certs/ca-certificates.crt
+
+    find | cpio -H newc -o | gzip -9 > ${output_dir}/${initrd_name}
+    popd
+}
+
+
+if [ ! -z ${INITRD_FILE} ]; then
+    cp ${INITRD_FILE} ${WORK_DIR}/
+    rebuild_initrd $(basename ${INITRD_FILE}) ${DAPPER_SOURCE}/build/
+    exit 0
+fi
+
 # copy the iso content
 mount -t iso9660 -o loop ${ISO_FILE} ${ORIGIN_DIR}
 cp -rf ${ORIGIN_DIR}/* ${NEW_DIR}
@@ -44,17 +76,7 @@ cp -rf ${ORIGIN_DIR}/* ${NEW_DIR}
 INITRD_NAME=$(basename ${ORIGIN_DIR}/boot/initrd-*)
 cp ${ORIGIN_DIR}/boot/initrd-* ${WORK_DIR}/
 
-# update and rebuild the initrd
-pushd ${WORK_DIR}
-mv initrd-* ${INITRD_NAME}.gz
-gzip -d ${INITRD_NAME}.gz
-cpio -i -F ${INITRD_NAME}
-rm -f ${INITRD_NAME}
-
-cat ${CERT_FILE} >> ${WORK_DIR}/usr/etc/ssl/certs/ca-certificates.crt
-
-find | cpio -H newc -o | gzip -9 > ${NEW_DIR}/boot/${INITRD_NAME}
-popd
+rebuild_initrd ${INITRD_NAME} ${NEW_DIR}/boot
 
 pushd ${NEW_DIR}
 xorriso \
